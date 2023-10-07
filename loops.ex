1aa518c1685226c738f1871da68bbce44e3fc8e3
yusifk88 <amloyal4life@gmail.com>
2023-10-07 02:24:59 +0000

    added loop to sum and add content of list

--- loops.ex
+++ loops.ex
@@ -1,5 +1,8 @@
 defmodule Playground do
 
+
+#  looping through a list
+
     def loop(0), do: :ok
     
     
@@ -9,5 +12,17 @@ defmodule Playground do
         IO.puts(n)
 
     end
+
+
+# summing a list of items or an array
+
+    def sumList([]), do: 0
+
+
+    def sumList([head|tail]) do
+
+    head+sumList(tail)
+
+end
 
 end
