2ad437cf1277a2aa7a4aae9863d443ec5f54c1eb
yusifk88 <amloyal4life@gmail.com>
2023-10-07 23:55:26 +0000

    added enum functions for loops and filters

--- loops.ex
+++ loops.ex
@@ -4,10 +4,10 @@ defmodule Playground do
 #  looping through a list
 
     def loop(0), do: :ok
-    
-    
+
+
     def loop(n) do
-    
+
         loop(n-1)
         IO.puts(n)
 
@@ -22,6 +22,96 @@ defmodule Playground do
     def sumList([head|tail]) do
 
     head+sumList(tail)
+
+end
+
+
+
+#some partern algo
+
+def myAlgo([]), do: :ok
+
+def myAlgo(items) do
+
+    Enum.each(items,fn x -> IO.puts(x)  end)
+
+end
+
+
+
+def filterList([]), do: 0
+
+def filterList(items) do
+
+   myList = Enum.filter(items,fn item -> item>5  end)
+
+   Enum.each(myList,fn litsItem -> IO.write(getFibNumber(litsItem))  end)
+
+
+end
+
+
+def mapList(items) when not is_list(items) do
+    :error
+end
+
+def mapList(items) do
+    Enum.map(items,fn item -> item+2  end)
+
+end
+
+
+
+def forLoop(items) do
+
+
+    for item <-items do
+      item*2
+    end
+
+end
+
+
+
+def doubleForLoops(items1,items2) do
+
+
+    for item1<-items1,item2<-items2 do
+
+
+        {item1,item2,item1+item2}
+
+    end
+
+
+
+end
+
+
+def getFibNumber(number) when number <0, do: :error
+
+def getFibNumber(number), do: getFibNumber(number,1,0)
+defp getFibNumber(0,_,results), do: results
+defp getFibNumber(number,next,results) do
+getFibNumber(number-1,next+results,next)
+
+end
+
+
+def startCaculation(number) do
+
+#start = OS.system_time(:seconds)
+
+fib = getFibNumber(number)
+
+#finish = OS.system_time(:seconds)
+
+#diff = finish - start
+
+IO.puts("#{fib} ,")
+
+
+#IO.puts("it run for #{diff}")
 
 end
 
